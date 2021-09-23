8c24c074090a3b2894468502c787fc9ec303654f
knokko <knokogator@hotmail.com>
2021-09-23 11:04:10 +0200

    Add CustomWandValues

---
+++ shared-code/src/main/java/nl/knokko/customitems/item/CustomWandValues.java
@@ -0,0 +1,216 @@
+package nl.knokko.customitems.item;
+
+import nl.knokko.customitems.encoding.ItemEncoding;
+import nl.knokko.customitems.itemset.ProjectileReference;
+import nl.knokko.customitems.itemset.SItemSet;
+import nl.knokko.customitems.projectile.CustomProjectileValues;
+import nl.knokko.customitems.trouble.UnknownEncodingException;
+import nl.knokko.customitems.util.Checks;
+import nl.knokko.customitems.util.ProgrammingValidationException;
+import nl.knokko.customitems.util.Validation;
+import nl.knokko.customitems.util.ValidationException;
+import nl.knokko.util.bits.BitInput;
+import nl.knokko.util.bits.BitOutput;
+
+public class CustomWandValues extends CustomItemValues {
+
+    public static CustomWandValues load(
+            BitInput input, byte encoding, SItemSet itemSet
+    ) throws UnknownEncodingException {
+        // Note: initial item type doesn't matter because it will be overwritten right away
+        CustomWandValues result = new CustomWandValues(false, CustomItemType.DIAMOND_HOE);
+
+        if (encoding == ItemEncoding.ENCODING_WAND_9) {
+            result.load9(input, itemSet);
+            result.initDefaults9();
+        } else if (encoding == ItemEncoding.ENCODING_WAND_10) {
+            result.load10(input, itemSet);
+            result.initDefaults10();
+        } else {
+            throw new UnknownEncodingException("CustomWand", encoding);
+        }
+
+        if (itemSet.getSide() == SItemSet.Side.EDITOR) {
+            result.loadEditorOnlyProperties1(input, itemSet, true);
+        }
+
+        return result;
+    }
+
+    private ProjectileReference projectile;
+
+    private int cooldown;
+
+    /** If this is null, the wand doesn't need charges and is only limited by its cooldown */
+    private WandChargeValues charges;
+
+    private int amountPerShot;
+
+    public CustomWandValues(boolean mutable, CustomItemType initialItemType) {
+        super(mutable, initialItemType);
+
+        this.projectile = null;
+        this.cooldown = 40;
+        this.charges = null;
+        this.amountPerShot = 1;
+    }
+
+    public CustomWandValues(CustomWandValues toCopy, boolean mutable) {
+        super(toCopy, mutable);
+
+        this.projectile = toCopy.getProjectileReference();
+        this.cooldown = toCopy.getCooldown();
+        this.charges = toCopy.getCharges();
+        this.amountPerShot = toCopy.getAmountPerShot();
+    }
+
+    @Override
+    public void save(BitOutput output, SItemSet.Side side) {
+        output.addByte(ItemEncoding.ENCODING_WAND_10);
+        save10(output);
+
+        if (side == SItemSet.Side.EDITOR) {
+            saveEditorOnlyProperties1(output);
+        }
+    }
+
+    private void save10(BitOutput output) {
+        saveIdentityProperties10(output);
+        saveTextDisplayProperties1(output);
+        saveVanillaBasedPowers4(output);
+        saveItemFlags6(output);
+        savePotionProperties10(output);
+        saveRightClickProperties10(output);
+        saveWandOnlyProperties9(output);
+        saveExtraProperties10(output);
+    }
+
+    private void saveWandOnlyProperties9(BitOutput output) {
+        output.addString(projectile.get().getName());
+        output.addInt(cooldown);
+        output.addBoolean(charges != null);
+        if (charges != null) {
+            charges.save1(output);
+        }
+        output.addInt(amountPerShot);
+    }
+
+    @Override
+    public byte getMaxStacksize() {
+        return 1;
+    }
+
+    @Override
+    public CustomWandValues copy(boolean mutable) {
+        return new CustomWandValues(this, mutable);
+    }
+
+    private void load9(BitInput input, SItemSet itemSet) {
+        loadIdentityProperties1(input);
+        loadTextDisplayProperties1(input);
+        loadVanillaBasedPowers4(input);
+        loadItemFlags6(input);
+        loadPotionProperties9(input);
+        loadRightClickProperties9(input);
+        loadWandOnlyProperties9(input, itemSet);
+    }
+
+    private void load10(BitInput input, SItemSet itemSet) throws UnknownEncodingException {
+        loadIdentityProperties10(input);
+        loadTextDisplayProperties1(input);
+        loadVanillaBasedPowers4(input);
+        loadItemFlags6(input);
+        loadPotionProperties10(input);
+        loadRightClickProperties10(input, itemSet);
+        loadWandOnlyProperties9(input, itemSet);
+        loadExtraProperties10(input);
+    }
+
+    private void loadWandOnlyProperties9(BitInput input, SItemSet itemSet) {
+        this.projectile = itemSet.getProjectileReference(input.readString());
+        this.cooldown = input.readInt();
+        if (input.readBoolean()) {
+            this.charges = WandChargeValues.load1(input);
+        } else {
+            this.charges = null;
+        }
+        this.amountPerShot = input.readInt();
+    }
+
+    private void initDefaults10() {
+        initBaseDefaults10();
+        initWandOnlyDefaults10();
+    }
+
+    private void initWandOnlyDefaults10() {
+        // There is nothing to be done until the next encoding
+    }
+
+    private void initDefaults9() {
+        initBaseDefaults9();
+        initWandOnlyDefaults9();
+    }
+
+    private void initWandOnlyDefaults9() {
+        initWandOnlyDefaults10();
+        // No new wand-only properties were added in encoding 10
+    }
+
+    public ProjectileReference getProjectileReference() {
+        return projectile;
+    }
+
+    public CustomProjectileValues getProjectile() {
+        return projectile.get();
+    }
+
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    public WandChargeValues getCharges() {
+        return charges;
+    }
+
+    public int getAmountPerShot() {
+        return amountPerShot;
+    }
+
+    public void setProjectile(ProjectileReference newProjectile) {
+        assertMutable();
+        Checks.notNull(newProjectile);
+        this.projectile = newProjectile;
+    }
+
+    public void setCooldown(int newCooldown) {
+        assertMutable();
+        this.cooldown = newCooldown;
+    }
+
+    public void setCharges(WandChargeValues newCharges) {
+        assertMutable();
+        this.charges = newCharges;
+    }
+
+    public void setAmountPerShot(int newAmountPerShot) {
+        assertMutable();
+        this.amountPerShot = newAmountPerShot;
+    }
+
+    @Override
+    public void validateIndependent() throws ValidationException, ProgrammingValidationException {
+        super.validateIndependent();
+
+        if (projectile == null) throw new ValidationException("You must choose a projectile");
+        if (cooldown < 1) throw new ValidationException("Cooldown must be positive");
+        if (charges != null) Validation.scope("Charges", charges::validate);
+        if (amountPerShot < 1) throw new ValidationException("Amount per shot must be positive");
+    }
+
+    @Override
+    public void validateComplete(SItemSet itemSet, String oldName) throws ValidationException, ProgrammingValidationException {
+        super.validateComplete(itemSet, oldName);
+
+        if (!itemSet.isReferenceValid(projectile)) throw new ProgrammingValidationException("Projectile is no longer valid");
+    }
+}
---
+++ shared-code/src/main/java/nl/knokko/customitems/item/WandChargeValues.java
@@ -0,0 +1,68 @@
+package nl.knokko.customitems.item;
+
+import nl.knokko.customitems.model.ModelValues;
+import nl.knokko.customitems.util.ValidationException;
+import nl.knokko.util.bits.BitInput;
+import nl.knokko.util.bits.BitOutput;
+
+public class WandChargeValues extends ModelValues {
+
+    public static WandChargeValues load1(BitInput input) {
+        WandChargeValues result = new WandChargeValues(false);
+
+        result.maxCharges = input.readInt();
+        result.rechargeTime = input.readInt();
+
+        return result;
+    }
+
+    private int maxCharges;
+    private int rechargeTime;
+
+    public WandChargeValues(boolean mutable) {
+        super(mutable);
+
+        this.maxCharges = 3;
+        this.rechargeTime = 60;
+    }
+
+    public WandChargeValues(WandChargeValues toCopy, boolean mutable) {
+        super(mutable);
+
+        this.maxCharges = toCopy.getMaxCharges();
+        this.rechargeTime = toCopy.getRechargeTime();
+    }
+
+    @Override
+    public WandChargeValues copy(boolean mutable) {
+        return new WandChargeValues(this, mutable);
+    }
+
+    public void save1(BitOutput output) {
+        output.addInt(maxCharges);
+        output.addInt(rechargeTime);
+    }
+
+    public int getMaxCharges() {
+        return maxCharges;
+    }
+
+    public int getRechargeTime() {
+        return rechargeTime;
+    }
+
+    public void setMaxCharges(int newMaxCharges) {
+        assertMutable();
+        this.maxCharges = newMaxCharges;
+    }
+
+    public void setRechargeTime(int newRechargeTime) {
+        assertMutable();
+        this.rechargeTime = newRechargeTime;
+    }
+
+    public void validate() throws ValidationException {
+        if (maxCharges <= 1) throw new ValidationException("Maximum charges must be greater than 1");
+        if (rechargeTime <= 0) throw new ValidationException("Recharge time must be positive");
+    }
+}
