635c72e20a9848a80d5089d955797b7a4ce2f22c
knokko <knokogator@hotmail.com>
2021-11-30 15:38:11 +0100

    Repair backward compatibility test 2

--- shared-code/src/test/java/nl/knokko/customitems/serialization/Backward2.java
+++ shared-code/src/test/java/nl/knokko/customitems/serialization/Backward2.java
@@ -24,14 +24,14 @@ public class Backward2 {
 
         testSimpleDefault2((SimpleCustomItemValues) set.getItem("simple1").get());
 
-        testSimpleItem2(set.getItem("simple2").get());
-        testSword1((CustomToolValues) set.getItem("sword1").get());
-        testPickaxe1((CustomToolValues) set.getItem("pickaxe1").get());
-        testAxe1((CustomToolValues) set.getItem("axe1").get());
-        testShovel1((CustomToolValues) set.getItem("shovel1").get());
+        testSimpleItem2(set.getItem("simple2").get(), set.getSide());
+        testSword1((CustomToolValues) set.getItem("sword1").get(), set.getSide());
+        testPickaxe1((CustomToolValues) set.getItem("pickaxe1").get(), set.getSide());
+        testAxe1((CustomToolValues) set.getItem("axe1").get(), set.getSide());
+        testShovel1((CustomToolValues) set.getItem("shovel1").get(), set.getSide());
     }
 
-    private static void testSimpleItem2(CustomItemValues item) {
+    private static void testSimpleItem2(CustomItemValues item, SItemSet.Side side) {
         assertEquals("simple2", item.getName());
         assertEquals(CustomItemType.DIAMOND_HOE, item.getItemType());
         // Internal item damage is no longer relevant
@@ -48,10 +48,14 @@ public class Backward2 {
                         1.0
                 )
         ), item.getAttributeModifiers());
-        assertEquals("gun1", item.getTexture().getName());
+        if (side == SItemSet.Side.EDITOR) {
+            assertEquals("gun1", item.getTexture().getName());
+        } else {
+            assertNull(item.getTextureReference());
+        }
     }
 
-    private static void testSword1(CustomToolValues item) {
+    private static void testSword1(CustomToolValues item, SItemSet.Side side) {
         assertEquals("sword1", item.getName());
         assertEquals(CustomItemType.STONE_SWORD, item.getItemType());
         // Internal item damage is no longer relevant
@@ -67,12 +71,16 @@ public class Backward2 {
                         3.0
                 )
         ), item.getAttributeModifiers());
-        assertEquals("test1", item.getTexture().getName());
+        if (side == SItemSet.Side.EDITOR) {
+            assertEquals("test1", item.getTexture().getName());
+        } else {
+            assertNull(item.getTextureReference());
+        }
         assertFalse(item.allowEnchanting());
         assertEquals(53L, (long) item.getMaxDurabilityNew());
     }
 
-    private static void testPickaxe1(CustomToolValues item) {
+    private static void testPickaxe1(CustomToolValues item, SItemSet.Side side) {
         assertEquals("pickaxe1", item.getName());
         // It looks like a bug in Editor 2.0 somehow turned the type from GOLD to IRON conditionally...
         //assertEquals(CustomItemType.IRON_PICKAXE, item.getItemType());
@@ -81,12 +89,16 @@ public class Backward2 {
                 "A pickaxe... but made of gold!"
         ), item.getLore());
         assertEquals(0, item.getAttributeModifiers().size());
-        assertEquals("gun1", item.getTexture().getName());
+        if (side == SItemSet.Side.EDITOR) {
+            assertEquals("gun1", item.getTexture().getName());
+        } else {
+            assertNull(item.getTextureReference());
+        }
         assertTrue(item.allowEnchanting());
         assertNull(item.getMaxDurabilityNew());
     }
 
-    private static void testAxe1(CustomToolValues item) {
+    private static void testAxe1(CustomToolValues item, SItemSet.Side side) {
         assertEquals("axe1", item.getName());
         assertEquals(CustomItemType.IRON_AXE, item.getItemType());
         assertEquals("Sharp Iron Axe", item.getDisplayName());
@@ -99,18 +111,26 @@ public class Backward2 {
                         7.0
                 )
         ), item.getAttributeModifiers());
-        assertEquals("test1", item.getTexture().getName());
+        if (side == SItemSet.Side.EDITOR) {
+            assertEquals("test1", item.getTexture().getName());
+        } else {
+            assertNull(item.getTextureReference());
+        }
         assertTrue(item.allowEnchanting());
         assertEquals(500, (long) item.getMaxDurabilityNew());
     }
 
-    private static void testShovel1(CustomToolValues item) {
+    private static void testShovel1(CustomToolValues item, SItemSet.Side side) {
         assertEquals("shovel1", item.getName());
         assertEquals(CustomItemType.DIAMOND_SHOVEL, item.getItemType());
         assertEquals("Crystal Shovel", item.getDisplayName());
         assertEquals(0, item.getLore().size());
         assertEquals(0, item.getAttributeModifiers().size());
-        assertEquals("gun1", item.getTexture().getName());
+        if (side == SItemSet.Side.EDITOR) {
+            assertEquals("gun1", item.getTexture().getName());
+        } else {
+            assertNull(item.getTextureReference());
+        }
         assertFalse(item.allowEnchanting());
         assertEquals(5000, (long) item.getMaxDurabilityNew());
     }
