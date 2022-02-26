6113e5c1f395a84d48545e774cbca0478c2a3e75
knokko <knokogator@hotmail.com>
2022-02-26 10:04:35 +0100

    Let /kci debug check whether you changed the resourcepack-url or resource-pack-sha1 since server startup. Closes #156

--- plug-in/src/main/java/nl/knokko/customitems/plugin/command/CommandCustomItems.java
+++ plug-in/src/main/java/nl/knokko/customitems/plugin/command/CommandCustomItems.java
@@ -32,10 +32,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
@@ -80,10 +77,37 @@ public class CommandCustomItems implements CommandExecutor {
 
 	private final ItemSetWrapper itemSet;
 	private final LanguageFile lang;
+
+	private final String initialResourcePackURL;
+	private final String initialResourcePackSHA1;
 	
 	public CommandCustomItems(ItemSetWrapper itemSet, LanguageFile lang) {
 		this.itemSet = itemSet;
 		this.lang = lang;
+
+		File serverProperties = new File("server.properties");
+		final String RESOURCE_PACK = "resource-pack=";
+		final String RESOURCE_PACK_HASH = "resource-pack-sha1=";
+
+		String resourcePackUrl = null;
+		String resourcePackHash = null;
+		try {
+			Scanner scanner = new Scanner(serverProperties);
+			while (scanner.hasNextLine()) {
+				String currentLine = scanner.nextLine();
+				if (currentLine.startsWith(RESOURCE_PACK)) {
+					resourcePackUrl = currentLine.substring(RESOURCE_PACK.length()).replace("\\", "");
+				} else if (currentLine.startsWith(RESOURCE_PACK_HASH)) {
+					resourcePackHash = currentLine.substring(RESOURCE_PACK_HASH.length());
+				}
+			}
+			scanner.close();
+		} catch (IOException ioTrouble) {
+			Bukkit.getLogger().warning("Can't find server.properties. This will reduce quality of /kci debug");
+		}
+
+		this.initialResourcePackURL = resourcePackUrl;
+		this.initialResourcePackSHA1 = resourcePackHash;
 	}
 
 	private void sendGiveUseage(CommandSender sender) {
@@ -422,7 +446,15 @@ public class CommandCustomItems implements CommandExecutor {
 																		+ "server resourcepack. The maximum size for these versions is "
 																		+ "50MiB.");
 															} else {
-																sender.sendMessage(ChatColor.GREEN + "Your server resourcepack seems fine.");
+																if (!Objects.equals(resourcePackUrl, initialResourcePackURL) || !Objects.equals(resourcePackHash, initialResourcePackSHA1)) {
+																	sender.sendMessage(
+																			ChatColor.YELLOW + "It looks like you changed the resourcepack url and/or resourcepack sha1 " +
+																					"in your server.properties, but these changes will only take effect after you restart " +
+																					"your server."
+																	);
+																} else {
+																	sender.sendMessage(ChatColor.GREEN + "Your server resourcepack seems fine.");
+																}
 															}
 														} else {
 															if (resourcePackHash.isEmpty()) {
