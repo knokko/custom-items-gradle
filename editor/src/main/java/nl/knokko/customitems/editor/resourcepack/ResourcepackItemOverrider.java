a23f01a217ee07c17276544efc5c9ad5e825b626
knokko <knokogator@hotmail.com>
2022-05-15 11:52:46 +0200

    Also fix overriding shields and bows in MC 1.12.2

--- editor/src/main/java/nl/knokko/customitems/editor/resourcepack/ResourcepackItemOverrider.java
+++ editor/src/main/java/nl/knokko/customitems/editor/resourcepack/ResourcepackItemOverrider.java
@@ -155,7 +155,11 @@ class ResourcepackItemOverrider {
         jsonWriter.println("{");
         jsonWriter.println("    \"parent\": \"item/generated\",");
         jsonWriter.println("    \"textures\": {");
-        jsonWriter.println("        \"layer0\": \"item/bow\"");
+        if (this.mcVersion >= VERSION1_13) {
+            jsonWriter.println("        \"layer0\": \"item/bow\"");
+        } else {
+            jsonWriter.println("        \"layer0\": \"items/bow\"");
+        }
         jsonWriter.println("    },");
 
         // Display
@@ -286,7 +290,9 @@ class ResourcepackItemOverrider {
         jsonWriter.println("{");
         jsonWriter.println("    \"parent\": \"builtin/entity\",");
         jsonWriter.println("    \"textures\": {");
-        jsonWriter.println("        \"particle\": \"block/dark_oak_planks\"");
+        if (this.mcVersion >= VERSION1_13) {
+            jsonWriter.println("        \"particle\": \"block/dark_oak_planks\"");
+        }
         jsonWriter.println("    },");
         jsonWriter.println("    \"display\": {");
 
