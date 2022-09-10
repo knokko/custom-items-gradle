2452ffa9f6cdb9a299cf93467994c4300b0f2f61
knokko <knokogator@hotmail.com>
2022-09-10 14:51:36 +0200

    Add world generation to the wiki generator

--- editor/src/main/java/nl/knokko/customitems/editor/wiki/WikiBlockGenerator.java
+++ editor/src/main/java/nl/knokko/customitems/editor/wiki/WikiBlockGenerator.java
@@ -1,14 +1,25 @@
 package nl.knokko.customitems.editor.wiki;
 
+import nl.knokko.customitems.NameHelper;
 import nl.knokko.customitems.block.CustomBlockValues;
 import nl.knokko.customitems.block.drop.CustomBlockDropValues;
+import nl.knokko.customitems.drops.AllowedBiomesValues;
+import nl.knokko.customitems.drops.CIBiome;
 import nl.knokko.customitems.editor.wiki.item.ItemDropGenerator;
+import nl.knokko.customitems.item.CIMaterial;
 import nl.knokko.customitems.item.CustomBlockItemValues;
 import nl.knokko.customitems.item.CustomItemValues;
+import nl.knokko.customitems.itemset.BlockReference;
 import nl.knokko.customitems.itemset.ItemSet;
+import nl.knokko.customitems.util.Chance;
+import nl.knokko.customitems.worldgen.BlockProducerValues;
+import nl.knokko.customitems.worldgen.CITreeType;
+import nl.knokko.customitems.worldgen.OreVeinGeneratorValues;
+import nl.knokko.customitems.worldgen.TreeGeneratorValues;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -62,6 +73,120 @@ class WikiBlockGenerator {
                 }
                 output.println("\t\t</ul>");
             }
+
+            boolean isGeneratedInTrees = itemSet.getTreeGenerators().stream().anyMatch(
+                    generator -> canProduceBlock(generator.getLogMaterial()) || canProduceBlock(generator.getLeavesMaterial())
+            );
+            boolean isGeneratedAsOre = itemSet.getOreVeinGenerators().stream().anyMatch(
+                    generator -> canProduceBlock(generator.getOreMaterial())
+            );
+            if (isGeneratedInTrees || isGeneratedAsOre) {
+                output.println("\t\t<h2>World generation</h2>");
+
+                if (isGeneratedInTrees) {
+                    output.println("\t\t<h3>Trees</h3>");
+                    output.println("\t\t<ul>");
+                    for (TreeGeneratorValues tree : itemSet.getTreeGenerators()) {
+                        if (canProduceBlock(tree.getLogMaterial())) {
+                            generateTreeGenerationInfo(output, tree, tree.getLogMaterial(), "log");
+                        }
+                        if (canProduceBlock(tree.getLeavesMaterial())) {
+                            generateTreeGenerationInfo(output, tree, tree.getLeavesMaterial(), "leaves");
+                        }
+                    }
+                    output.println("\t\t</ul>");
+                }
+
+                if (isGeneratedAsOre) {
+                    output.println("\t\t<h3>Ore</h3>");
+                    output.println("\t\t<ul>");
+
+                    for (OreVeinGeneratorValues generator : itemSet.getOreVeinGenerators()) {
+                        if (canProduceBlock(generator.getOreMaterial())) {
+                            output.println("\t\t\t<li>");
+                            output.println("\t\t\t\tThis block has " + getGenerationChance(generator.getOreMaterial())
+                                    + " chance to be generated in ore veins that can replace the following blocks:");
+                            output.println("\t\t\t\t<ul>");
+                            for (CIMaterial vanillaBlock : generator.getBlocksToReplace().getVanillaBlocks()) {
+                                output.println("\t\t\t\t\t<li>" + NameHelper.getNiceEnumName(vanillaBlock.name()) + "</li>");
+                            }
+                            for (BlockReference customBlock : generator.getBlocksToReplace().getCustomBlocks()) {
+                                output.println("\t\t\t\t\t<li><a href=\"" + customBlock.get().getName() + ".html\">"
+                                        + customBlock.get().getName() + "</a></li>");
+                            }
+                            output.println("\t\t\t\t</ul>");
+                            output.println("\t\t\t\tThese ore veins can only be generated between Y=" + generator.getMinY()
+                                    + " and Y=" + generator.getMaxY() + ".<br>");
+                            output.println("\t\t\t\tThere is " + generator.getChance() + " chance that " + generator.getMinNumVeins()
+                                    + " to " + generator.getMaxNumVeins() + " veins will be spawned per chunk.<br>");
+                            output.println("\t\t\t\tThe expected size of these veins is " + generator.getMinVeinSize()
+                                    + " to " + generator.getMaxVeinSize() + " blocks.<br>");
+                            generateAllowedBiomes(output, "\t\t\t\t", generator.getAllowedBiomes());
+                            output.println("\t\t\t</li>");
+                        }
+                    }
+
+                    output.println("\t\t</ul>");
+                }
+            }
         });
+    }
+
+    private void generateTreeGenerationInfo(
+            PrintWriter output, TreeGeneratorValues tree,
+            BlockProducerValues producer, String description
+    ) {
+        output.println("\t\t\t<li>");
+        output.println("\t\t\t\tThis block has " + getGenerationChance(producer)
+                + " chance to be generated as " + description + " of custom " + getNiceTreeName(tree.getTreeType()) + ".");
+        generateAllowedBiomes(output, "\t\t\t\t", tree.getAllowedBiomes());
+        output.println("\t\t\t</li>");
+    }
+
+    private void generateAllowedBiomes(PrintWriter output, String tabs, AllowedBiomesValues biomes) {
+        if (biomes.getWhitelist().isEmpty()) {
+            if (biomes.getBlacklist().isEmpty()) {
+                output.println(tabs + "These can be generated in all biomes.");
+            } else {
+                output.println(tabs + "These can be generated in all biomes, except");
+                generateBiomeList(output, tabs, biomes.getBlacklist());
+            }
+        } else {
+            output.println(tabs + "These can be generated in the following biomes:");
+            generateBiomeList(output, tabs, biomes.getWhitelist());
+            if (!biomes.getBlacklist().isEmpty()) {
+                output.println(tabs + "except");
+                generateBiomeList(output, tabs, biomes.getBlacklist());
+            }
+        }
+    }
+
+    private void generateBiomeList(PrintWriter output, String tabs, Collection<CIBiome> biomes) {
+        output.println(tabs + "<ul>");
+        for (CIBiome forbiddenBiome : biomes) {
+            output.println(tabs + "\t<li>" + NameHelper.getNiceEnumName(forbiddenBiome.name()) + "</li>");
+        }
+        output.println(tabs + "</ul>");
+    }
+
+    private String getNiceTreeName(CITreeType treeType) {
+        if (treeType.name().contains("TREE")) {
+            return NameHelper.getNiceEnumName(treeType.name()) + "s";
+        } else {
+            return NameHelper.getNiceEnumName(treeType.name()) + " trees";
+        }
+    }
+
+    private Chance getGenerationChance(BlockProducerValues producer) {
+        for (BlockProducerValues.Entry entry : producer.getEntries()) {
+            if (entry.getBlock().isCustom() && entry.getBlock().getCustomBlock().get().getInternalID() == block.getInternalID()) {
+                return entry.getChance();
+            }
+        }
+        return null;
+    }
+
+    private boolean canProduceBlock(BlockProducerValues producer) {
+        return getGenerationChance(producer) != null;
     }
 }
