4fd68aa5a73a54cfa58e0c22d3b1588ee4771056
knokko <knokogator@hotmail.com>
2022-11-26 17:05:31 +0100

    Remove license headers from source files (because they were not maintained anyway)

--- core-plugin-12/src/main/java/nl/knokko/core/plugin/command/CommandTest.java
+++ core-plugin-12/src/main/java/nl/knokko/core/plugin/command/CommandTest.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
- *  This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 package nl.knokko.core.plugin.command;
 
 import org.bukkit.Bukkit;
