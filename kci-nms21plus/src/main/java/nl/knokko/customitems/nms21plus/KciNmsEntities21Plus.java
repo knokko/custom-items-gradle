4be54d769780012746e45b1c32f3bd1ab946e1a8
knokko <knokogator@hotmail.com>
2025-08-10 21:49:41 +0200

    Fix 1.21 projectile damage

--- kci-nms21plus/src/main/java/nl/knokko/customitems/nms21plus/KciNmsEntities21Plus.java
+++ kci-nms21plus/src/main/java/nl/knokko/customitems/nms21plus/KciNmsEntities21Plus.java
@@ -4,6 +4,7 @@ import nl.knokko.customitems.nms16plus.KciNmsEntities16Plus;
 import org.bukkit.damage.DamageSource;
 import org.bukkit.damage.DamageType;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Fireball;
 import org.bukkit.entity.LivingEntity;
 
 public class KciNmsEntities21Plus extends KciNmsEntities16Plus {
@@ -11,8 +12,13 @@ public class KciNmsEntities21Plus extends KciNmsEntities16Plus {
 	@Override
 	@SuppressWarnings("UnstableApiUsage")
 	public void causeFakeProjectileDamage(Entity toDamage, LivingEntity responsibleShooter, float damage, double projectilePositionX, double projectilePositionY, double projectilePositionZ, double projectileMotionX, double projectileMotionY, double projectileMotionZ) {
-		DamageSource damageSource = DamageSource.builder(DamageType.FIREBALL).withCausingEntity(responsibleShooter).build();
+		Fireball fakeProjectile = toDamage.getWorld().spawn(toDamage.getLocation(), Fireball.class);
+		fakeProjectile.setShooter(responsibleShooter);
+		DamageSource damageSource = DamageSource.builder(DamageType.FIREBALL)
+				.withCausingEntity(responsibleShooter)
+				.withDirectEntity(fakeProjectile).build();
 		((LivingEntity) toDamage).damage(damage, damageSource);
+		fakeProjectile.remove();
 	}
 
 	@Override
