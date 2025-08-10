43287ced0bc32fa0f19c7a346384e989d147e8b2
knokko <knokogator@hotmail.com>
2025-08-10 17:42:48 +0200

    test translations 1

--- kci-nms21/src/main/java/nl/knokko/customitems/nms21/KciNmsItems21.java
+++ kci-nms21/src/main/java/nl/knokko/customitems/nms21/KciNmsItems21.java
@@ -1,8 +1,10 @@
 package nl.knokko.customitems.nms21;
 
+import net.kyori.adventure.text.Component;
 import nl.knokko.customitems.nms20plus.KciNmsItems20Plus;
+//import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
-import org.bukkit.craftbukkit.v1_21_R5.inventory.CraftItemStack;
+//import org.bukkit.craftbukkit.v1_21_R5.inventory.CraftItemStack;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -11,8 +13,10 @@ import org.bukkit.inventory.meta.components.EquippableComponent;
 public class KciNmsItems21 extends KciNmsItems20Plus {
     @Override
     public String getStackName(ItemStack stack) {
-        net.minecraft.world.item.ItemStack nms = CraftItemStack.asNMSCopy(stack);
-        return nms.y().getString();
+//        stack.getItemMeta().getItemName();
+//        net.minecraft.world.item.ItemStack nms = CraftItemStack.asNMSCopy(stack);
+//        return nms.y().getString();
+        return "test1234";
     }
 
     @Override
@@ -22,5 +26,15 @@ public class KciNmsItems21 extends KciNmsItems20Plus {
         component.setModel(new NamespacedKey("minecraft", id));
         component.setSlot(slot);
         meta.setEquippable(component);
+    }
+
+    @Override
+    public ItemStack translate(ItemStack item, String itemName, boolean translateDisplayName, int loreSize) {
+        if (!item.editMeta(meta -> {
+            meta.customName(Component.text("test1234"));
+        })) {
+            throw new RuntimeException("Failed to edit ItemMeta of " + item + " for " + itemName);
+        }
+        return item;
     }
 }
