b8f2653b35091842585f51856033eb9ce024a01d
knokko <knokogator@hotmail.com>
2025-08-10 17:40:38 +0200

    Simplify causeFakeProjectileDamage

--- kci-nms21/src/main/java/nl/knokko/customitems/nms21/KciNmsEntities21.java
+++ kci-nms21/src/main/java/nl/knokko/customitems/nms21/KciNmsEntities21.java
@@ -1,31 +1,17 @@
 package nl.knokko.customitems.nms21;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.MinecraftKey;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.projectile.EntityTippedArrow;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import nl.knokko.customitems.nms20plus.KciNmsEntities20Plus;
-import org.bukkit.craftbukkit.v1_21_R5.CraftWorld;
-import org.bukkit.craftbukkit.v1_21_R5.entity.CraftEntity;
-import org.bukkit.craftbukkit.v1_21_R5.entity.CraftLivingEntity;
+import org.bukkit.damage.DamageSource;
+import org.bukkit.damage.DamageType;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
 public class KciNmsEntities21 extends KciNmsEntities20Plus {
+
     @Override
+    @SuppressWarnings("UnstableApiUsage")
     public void causeFakeProjectileDamage(Entity toDamage, LivingEntity responsibleShooter, float damage, double projectilePositionX, double projectilePositionY, double projectilePositionZ, double projectileMotionX, double projectileMotionY, double projectileMotionZ) {
-        Item arrowItem = BuiltInRegistries.g.a(MinecraftKey.a("arrow"));
-        Item bowItem = BuiltInRegistries.g.a(MinecraftKey.a("bow"));
-        EntityTippedArrow fakeArrow = new EntityTippedArrow(((CraftWorld) toDamage.getWorld()).getHandle(),
-                projectilePositionX, projectileMotionY, projectileMotionZ, new ItemStack(arrowItem), new ItemStack(bowItem));
-        fakeArrow.b(((CraftLivingEntity) responsibleShooter).getHandle());
-        fakeArrow.projectileSource = responsibleShooter;
-
-        net.minecraft.world.entity.Entity nmsEntity = ((CraftEntity) toDamage).getHandle();
-        DamageSource indirectDamageSource = nmsEntity.ea().a(fakeArrow, ((CraftEntity) responsibleShooter).getHandle());
-
-        nmsEntity.a(((CraftWorld) toDamage.getWorld()).getHandle(), indirectDamageSource, damage);
+        DamageSource damageSource = DamageSource.builder(DamageType.FIREBALL).withCausingEntity(responsibleShooter).build();
+        ((LivingEntity) toDamage).damage(damage, damageSource);
     }
 }
