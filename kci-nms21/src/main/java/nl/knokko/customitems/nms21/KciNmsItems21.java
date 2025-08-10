f6392389a2a319ccfa321af9ba5bf70e344d1d53
knokko <knokogator@hotmail.com>
2025-08-10 18:39:52 +0200

    test translations 3

--- kci-nms21/src/main/java/nl/knokko/customitems/nms21/KciNmsItems21.java
+++ kci-nms21/src/main/java/nl/knokko/customitems/nms21/KciNmsItems21.java
@@ -3,12 +3,17 @@ package nl.knokko.customitems.nms21;
 import net.kyori.adventure.text.Component;
 import nl.knokko.customitems.nms20plus.KciNmsItems20Plus;
 //import org.bukkit.Bukkit;
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 //import org.bukkit.craftbukkit.v1_21_R5.inventory.CraftItemStack;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.components.EquippableComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 
 public class KciNmsItems21 extends KciNmsItems20Plus {
     @Override
@@ -31,9 +36,16 @@ public class KciNmsItems21 extends KciNmsItems20Plus {
     @Override
     public ItemStack translate(ItemStack item, String itemName, boolean translateDisplayName, int loreSize) {
         if (!item.editMeta(meta -> {
-            meta.customName(item.effectiveName().append(Component.translatable("kci." + itemName + ".name")));
+            if (translateDisplayName) {
+                meta.customName(Component.translatable("kci." + itemName + ".name"));
+            }
+            List<Component> loreComponents = new ArrayList<>(loreSize);
+            for (int index = 0; index < loreSize; index++) {
+                loreComponents.add(Component.translatable("kci." + itemName + ".lore." + index));
+            }
+            meta.lore(loreComponents);
         })) {
-            throw new RuntimeException("Failed to edit ItemMeta of " + item + " for " + itemName);
+            Bukkit.getLogger().log(Level.WARNING, "Failed to translate custom item " + itemName);
         }
         return item;
     }
